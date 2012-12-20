e6e1201526cadd607b97ce2576c7bc23fa378ee8
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2012-12-20 22:53:12 +0100

    Remove the dot

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 CWD="$(pwd)"
-FILES='.zshrc .zshenv .gitconfig .gitignore_global'
+FILES='zshrc zshenv gitconfig gitignore_global'
 
 for f in $FILES; do
-    cp -v -f -R ~/$f ~/$f.orig
-    ln -v -s -f $CWD/$f ~/$f
+    rm -f ~/.$f.orig
+    cp -f -R ~/.$f{,.orig}
+    ln -v -s -f $CWD/$f ~/.$f
 done
