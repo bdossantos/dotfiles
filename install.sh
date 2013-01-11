d02aa29f8af779f409477685fa7d31a72d667cbe
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2013-01-11 23:19:12 +0100

    Check if file exist before backup

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ FILES='zshrc zshenv gitconfig gitignore_global'
 
 for f in $FILES; do
     rm -f ~/.$f.orig
-    cp -f -R ~/.$f{,.orig}
+    if [ -f ~/.$f ]; then
+        cp -f -R ~/.$f{,.orig}
+    fi
     ln -v -s -f $CWD/$f ~/.$f
 done
