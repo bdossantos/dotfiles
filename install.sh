1bb60262c8fbbb7ab66e1ea6692837e499fd2bac
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2014-10-30 23:28:41 +0100

    Cleanup, fix coding style

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
 if ! which stow >/dev/null; then
-  echo "CAN I HAZ STOW ?"
+  echo 'CAN I HAZ STOW ?'
   exit 1
 fi
 
-script_name=$(basename $0)
+script_name=$(basename "$0")
 
-stow -S . -t $HOME -v --ignore='README.md' --ignore="$script_name"
-if [ $? -eq 0 ]; then
-  exec $SHELL
-fi
+stow -S . -t "$HOME" -v --ignore='README.md' --ignore="$script_name" && \
+  exec "$SHELL"
